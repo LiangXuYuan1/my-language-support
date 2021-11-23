53590335169d0cdcf3251e5a5759250f7e7ec630
旭源 <liangxuyuan@outlook.com>
2021-11-23 11:29:36 +0800

    把Token和Element类型定义移动到psi子包中，与示例保持一致

--- src/main/java/pers/liangxuyuan/language/psi/SimpleTokenType.java
+++ src/main/java/pers/liangxuyuan/language/psi/SimpleTokenType.java
@@ -1,8 +1,9 @@
-package pers.liangxuyuan.language;
+package pers.liangxuyuan.language.psi;
 
 import com.intellij.psi.tree.IElementType;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import pers.liangxuyuan.language.SimpleLanguage;
 
 public class SimpleTokenType extends IElementType {
     public SimpleTokenType(@NonNls @NotNull String debugName) {
