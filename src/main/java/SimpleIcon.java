ab66046760270980779975d29fc3e638197b5eef
旭源 <liangxuyuan@outlook.com>
2021-11-23 09:45:03 +0800

    定义Icon

---
+++ src/main/java/SimpleIcon.java
@@ -0,0 +1,7 @@
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class SimpleIcon {
+    public static final Icon FILE = IconLoader.getIcon("/icons/jar-gray.png", Icon.class);
+}
