3674ba708664beb1fd4a073cb5bd761590d1647a
旭源 <liangxuyuan@outlook.com>
2021-11-23 16:37:39 +0800

    实现Simple语言的语法高亮的颜色配置界面

---
+++ src/main/java/pers/liangxuyuan/language/SimpleColorSettingsPage.java
@@ -0,0 +1,69 @@
+package pers.liangxuyuan.language;
+
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.options.colors.AttributesDescriptor;
+import com.intellij.openapi.options.colors.ColorDescriptor;
+import com.intellij.openapi.options.colors.ColorSettingsPage;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.util.Map;
+
+public class SimpleColorSettingsPage implements ColorSettingsPage {
+    private static final AttributesDescriptor[] DESCRIPTORS = new AttributesDescriptor[]{
+            new AttributesDescriptor("Key", SimpleSyntaxHighlighter.KEY),
+            new AttributesDescriptor("Separator", SimpleSyntaxHighlighter.SEPARATOR),
+            new AttributesDescriptor("Value", SimpleSyntaxHighlighter.VALUE),
+            new AttributesDescriptor("Bad Value", SimpleSyntaxHighlighter.BAD_CHARACTER)
+    };
+
+
+    @Override
+    public @Nullable Icon getIcon() {
+        return SimpleIcons.FILE;
+    }
+
+    @Override
+    public @NotNull SyntaxHighlighter getHighlighter() {
+        return new SimpleSyntaxHighlighter();
+    }
+
+    @Override
+    public @NonNls @NotNull String getDemoText() {
+        return "# You are reading the \".properties\" entry.\n" +
+                "! The exclamation mark can also mark text as comments.\n" +
+                "website = https://en.wikipedia.org/\n" +
+                "language = English\n" +
+                "# The backslash below tells the application to continue reading\n" +
+                "# the value onto the next line.\n" +
+                "message = Welcome to \\\n" +
+                "          Wikipedia!\n" +
+                "# Add spaces to the key\n" +
+                "key\\ with\\ spaces = This is the value that could be looked up with the key \"key with spaces\".\n" +
+                "# Unicode\n" +
+                "tab : \\u0009";
+    }
+
+    @Override
+    public @Nullable Map<String, TextAttributesKey> getAdditionalHighlightingTagToDescriptorMap() {
+        return null;
+    }
+
+    @Override
+    public AttributesDescriptor @NotNull [] getAttributeDescriptors() {
+        return DESCRIPTORS;
+    }
+
+    @Override
+    public ColorDescriptor @NotNull [] getColorDescriptors() {
+        return ColorDescriptor.EMPTY_ARRAY;
+    }
+
+    @Override
+    public @NotNull String getDisplayName() {
+        return "Simple";
+    }
+}
